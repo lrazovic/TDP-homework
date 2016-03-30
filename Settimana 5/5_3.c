5551ec1e00d23b7f71bc101d70ae135dc433c43d
Leonardo Razovic <leorazzo@gmail.com>
2016-03-30 22:44:17 +0200

    Lavori sulla settimana 5

--- Settimana 5/5_3.c
+++ Settimana 5/5_3.c
@@ -2,34 +2,36 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    
+
     //Alloco memoria
     void *mem = malloc(sizeof(float)+sizeof(char));
-    
+
     //Valori di Gradi
     float *valori;
-	valori = (float*) ((char *)mem + sizeof(float));
-    
+	  valori = (float*) ((char *)mem + sizeof(float));
+
     //Scala usata
     char *scala;
-	scala = (char*) ((char *)mem + sizeof(char)); 
-    
+	  scala = (char*) ((char *)mem + sizeof(char));
+
     printf("C per Celsius, K per Kelvin e F per Fahrenheit \n");
     printf("Inserisci la scala da usare: \n");
     scanf("%c", scala);
     printf("Inserisci la temperatura: \n");
     scanf("%f", valori);
- 
-	if(*scala == 'c' ){
-	    printf("Kelvin: %f, F: %f \n", *valori + 273.15, (*valori *9/5) + 32);
-	}
 
-	else if (*scala == 'k'){
-        printf("Celsius: %f, F: %f \n", *valori - 273.15, ((*valori - 273.15) *9/5) +32);
-	}
+    if(*scala == 'c' ){
+      printf("Kelvin: %f, F: %f \n", *valori + 273.15, (*valori *9/5) + 32);
+    }
+
+    else if (*scala == 'k'){
+      printf("Celsius: %f, F: %f \n", *valori - 273.15, ((*valori - 273.15) *9/5) +32);
+    }
 
     else {
-        printf("Celsius: %f, Kelvin: %f \n", (*valori -32)*5/9, (*valori -32)*5/9 + 273.15);
+      printf("Celsius: %f, Kelvin: %f \n", (*valori -32)*5/9, (*valori -32)*5/9 + 273.15);
     }
+    
+    //Libero la memoria
     free(mem);
 }
