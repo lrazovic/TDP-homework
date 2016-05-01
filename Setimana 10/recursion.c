7f3384029cf817aeca28b56f3a7b9260a36f3ae5
Leonardo Razovic <Leonardo Razovic>
2016-05-01 17:19:26 +0200

    set10

---
+++ Setimana 10/recursion.c
@@ -0,0 +1,29 @@
+#include "recursion.h"
+
+int length(BinSeq* bin_seq) { 
+
+}
+
+void copy(BinSeq* dest, BinSeq* src) {
+
+}
+
+void concatenate(BinSeq* dest, BinSeq* src) { 
+
+}
+
+int indexOf(BinSeq* bin_seq, char c) { 
+
+}
+
+int equal(BinSeq* bin_seq1, BinSeq* bin_seq2) { 
+
+}
+
+int prefix(BinSeq* bin_seq, BinSeq* prefix) { 
+
+}
+
+int lengthMaxSequence(BinSeq* bin_seq, char c) { 
+
+}
